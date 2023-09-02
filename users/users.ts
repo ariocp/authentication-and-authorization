5954f7cfffd49625bf6d3e917c5b272d507ec26e
ariocp <ariocp@rambler.ru>
2023-09-03 01:22:56 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   index.ts
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   tsconfig.json
    	new file:   users/users.ts

---
+++ users/users.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const userRouter = express.Router()
+
+userRouter.use((req, res, next) => {
+    console.log('Обработчик users')
+    next()
+})
+
+userRouter.post('/register', (req, res) => {
+    res.send('register')
+})
+
+export { userRouter } 
