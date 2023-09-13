3d11d1a07d6107ad1937128659ed8a53d868a2fd
ariocp <ariocp@rambler.ru>
2023-09-13 20:40:29 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   nodemon.json
    	modified:   package-lock.json
    	modified:   package.json
    	renamed:    prisma/migrations/20230912211038_init/migration.sql -> prisma/migrations/20230913153857_init/migration.sql
    	modified:   prisma/migrations/migration_lock.toml
    	modified:   src/common/auth.middleware.ts
    	modified:   src/logger/logger.service.ts
    	renamed:    src/users/user.service.spec.ts -> src/users/users.service.spec.ts
    	modified:   tsconfig.json

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -1,19 +1,19 @@
 import { IMiddleware } from './middleware.interface';
 import { NextFunction, Request, Response } from 'express';
-import { verify } from 'jsonwebtoken';
+import { verify, Secret, VerifyOptions } from 'jsonwebtoken';
 
 export class AuthMiddleware implements IMiddleware {
-	constructor(private secret: string) {}
+	constructor(private secret: Secret) {}
 
 	execute(req: Request, res: Response, next: NextFunction): void {
 		if (req.headers.authorization) {
 			verify(req.headers.authorization.split(' ')[1], this.secret, (err, payload) => {
 				if (err) {
 					next();
-				} else if (payload) {
-					req.user = payload.email;
-					next();
+				} else if (typeof payload !== 'string' && payload?.email) {
+					req.user = payload?.email;
 				}
+				next();
 			});
 		} else {
 			next();
